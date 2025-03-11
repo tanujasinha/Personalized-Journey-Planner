abb0794fee5da21cfb0625b1011402d0a8191d2a
Tanuja Sinha <sinhatanuja123@gmail.com>
2025-03-11 21:25:55 +0530

    Database connected

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Initialize Express
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Welcome to Personalized Journey Planner API");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
