a82d9df1dd538f71fbe3d980e5bd79c33e83c0ca
Tanuja Sinha <sinhatanuja123@gmail.com>
2025-05-26 15:06:33 +0530

    bugs fixed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5000", credentials: true }));
+app.use(cors());
 // Serve static frontend files
 app.use(express.static(path.join(__dirname)));
 app.use(session({
